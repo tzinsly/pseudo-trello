f57bab651b6c0a2986c179c76a26cff345202ab9
Zinsly, Tatiane <tatianezinsly@gmail.com>
2020-03-24 21:32:56 +0100

    Adding Cards into list component

--- src/components/List.js
+++ src/components/List.js
@@ -3,20 +3,50 @@ import Card from './Card'
 import PropTypes from 'prop-types'
 
 class List extends React.Component {
-    render () {
-        return (
-            <div className="list">
-                <div className="list-header">
-                    <h2>{this.props.list.title}</h2>
-                </div>
-                {Object.keys(this.props.list.cards).map( key => (
-                    <Card
-                        key={key}
-                        cardData={this.props.list.cards[key]}/>
-                ))}
-            </div>
-        )
+
+  state = {
+    currentCards: []
+  }
+
+  newInput = React.createRef()
+  createNewCard = (e) => {
+    e.preventDefault()
+    const card = {
+      text: this.newInput.current.value,
+      listId: "123",
+      labels: [],
+      createdAt: new Date()
     }
+
+    if (card.text) {
+      this.setState({ currentCards: [...this.state.currentCards, card] })
+    }
+    
+    this.newInput.current.value = ''
+    console.log("Added Card successfully: " + card.text)
+  } 
+
+  render() {
+    return (
+      <div className="list">
+        <div className="list-header">
+          <h2>{this.props.list.title}</h2>
+        </div>
+        {Object.keys(this.props.list.cards).map(key => (
+          <Card
+            key={key}
+            cardData={this.props.list.cards[key]} />
+        ))}
+        <form onSubmit={this.createNewCard} className="new-card-wrapper">
+          <input
+            name="name"
+            type="text"
+            ref={this.newInput}
+            placeholder=" + New Card" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default List
